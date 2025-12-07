2e1df8045d10b8296ffa0e257922c6c7306f686c
jaroslaw-filipiak <info@j-filipiak.pl>
2025-12-07 18:20:13 +0100

    add tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   build: {
@@ -14,4 +15,5 @@ export default defineConfig({
       },
     },
   },
+  plugins: [tailwindcss()],
 });
